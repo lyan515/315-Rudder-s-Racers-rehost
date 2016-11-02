f13e732f87091e27d7b970fceaf68f85d5a20dd6
ameserole <ameserole@gmail.com>
2016-11-02 17:39:39 -0500

    Refactored current code to have a better file structure

---
+++ public/otherPlayer.js
@@ -0,0 +1,25 @@
+var OtherPlayer = function(id, game, player, startX, startY, startAngle) {
+	var x = startX;
+	var y = startY;
+	var angle = startAngle;
+	//game.load.image('bluebike', 'bluebike.png');
+
+	this.game = game;
+	this.player = player;
+	
+	this.player = game.add.sprite(x, y, 'bluebike');
+	this.player.id = id.toString();
+
+	this.player.anchor.setTo(0.5, 0.5);
+	this.player.scale.setTo(0.5, 0.5);
+	this.player.angle = angle;
+	this.lastPosition = { x: x, y: y, angle: angle };
+}
+
+OtherPlayer.prototype.update = function() {
+	this.lastPosition.x = this.player.x;
+	this.lastPosition.y = this.player.y;
+	this.lastPosition.angle = this.player.angle;
+}
+
+window.OtherPlayer = OtherPlayer;
