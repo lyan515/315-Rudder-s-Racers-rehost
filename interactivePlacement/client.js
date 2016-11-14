76049ac89b801f1b8d2660b792612750cef7603b
Travis Stebbins <stebbins.travis@compute-linux1.cs.tamu.edu>
2016-11-13 22:18:37 -0600

    Add comments and remove unnecessary overlap function

--- interactivePlacement/client.js
+++ interactivePlacement/client.js
@@ -1,33 +1,48 @@
+// width and height of single map panel image
 var MAPPANELWIDTH = 3840;
 var MAPPANELHEIGHT = 3347;
+// scale factor for game world
 var SCALEFACTOR = 2;
+// width and height of game world
+//	depend on map panel dimensions and scale factor
 var WORLDWIDTH = MAPPANELWIDTH * 2 * SCALEFACTOR;
 var WORLDHEIGHT = MAPPANELHEIGHT * 2 * SCALEFACTOR;
+// height of interactive placement program's window
 var WINDOWHEIGHT = 900;
 
+// scroll and zoom speed of camera
 var CAMERASCROLLSPEED = 10;
 var CAMERAZOOMSPEED = 0.1;
 
+// create Phaser game instance
 var game = new Phaser.Game(WINDOWHEIGHT * (WORLDWIDTH / WORLDHEIGHT), WINDOWHEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+// graphics object that will be used for drawing on the screen
 var graphics;
+var LINEWIDTH = 2;
 
 var socket;
 
+// arrays of objects that will be placed throughout the world
 var obstacles = [];
 var boundaries = [];
 var powerUps = [];
 
+// variables used for interactive placement system controls
 var cursors;
 var leftClicked = false;
 var rightClicked = false;
 var enterPressed = false;
 
+// tool options
 // 1 : boundaries, 2 : obstacles, 3 : power-ups
 var currentTool = 1;
 var tooltext;
 
+// stores the currently edited object
 var currentObject;
 var Rectangle = function () {
+	// raw values are for the interactive placement system
+	// scaled values are for the actual game client
 	var rawX = 0;
 	var rawY = 0;
 	var scaledX = 0;
@@ -41,7 +56,6 @@ var Rectangle = function () {
 function preload () {
 
 	game.load.image('obstacle', 'trashCan.png');
-	game.load.
 	// total map size: 7680 x 6694
 	game.load.image('mapTL', 'campusCircuit_TL.png');
 	game.load.image('mapTR', 'campusCircuit_TR.png');
@@ -77,7 +91,7 @@ function create () {
 	graphics = game.add.graphics(0, 0);
 	graphics.lineStyle(4, 0xff0000, 1);
 
-	// create text
+	// create tool text
 	toolText = game.add.text(32, 32, "Boundaries", {
 		font: "32px Arial",
 		fill: "#ffffff",
@@ -88,20 +102,12 @@ function create () {
     cursors = game.input.keyboard.createCursorKeys();
     game.input.mouse.mouseWheelCallback = mouseWheel;
 
+    // prevent context menu from appearing on right click
     game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
 }
 
 function toDegrees (angle) {
 	return angle * (180 / Math.PI);
-}
-
-function checkOverlap(spriteA, spriteB) {
-
-	var boundsA = spriteA.getBounds();
-	var boundsB = spriteB.getBounds();
-
-	return Phaser.Rectangle.intersects(boundsA, boundsB);
-
 }
 
 function update() {
@@ -134,27 +140,35 @@ function update() {
 		toolText.text = "Power-Ups";
 	}
 
-	// handle left clicks
+	// handle left click being pressed
 	if (game.input.activePointer.leftButton.isDown && !leftClicked) {
-		var rawX = game.input.activePointer.worldX;
-		var rawY = game.input.activePointer.worldY;
-		var scaledX = rawX * (WORLDHEIGHT / WINDOWHEIGHT) / game.world.scale.x;
-		var scaledY = rawY * (WORLDHEIGHT / WINDOWHEIGHT) / game.world.scale.y;
+		// get raw coordinates
+		var rawX = game.input.activePointer.worldX / game.world.scale.x;
+		var rawY = game.input.activePointer.worldY / game.world.scale.y;
+		// convert to scaled coordinates
+		var scaledX = rawX * (WORLDHEIGHT / WINDOWHEIGHT);
+		var scaledY = rawY * (WORLDHEIGHT / WINDOWHEIGHT);
+		// set leftClicked to true to register only one click
 		leftClicked = true;
-		x1 = game.input.activePointer.worldX / game.world.scale.x;
-		y1 = game.input.activePointer.worldY / game.world.scale.y;
+		// create new rectangle object and set initial coordinates
 		currentObject = new Rectangle();
-		currentObject.rawX = x1;
-		currentObject.rawY = y1;
+		currentObject.rawX = rawX;
+		currentObject.rawY = rawY;
 		currentObject.scaledX = scaledX;
 		currentObject.scaledY = scaledY;
 	}
+	// handle left click being released
 	else if (game.input.activePointer.leftButton.isUp && leftClicked) {
-		var rawX = game.input.activePointer.worldX;
-		var rawY = game.input.activePointer.worldY;
-		var scaledX = rawX * (WORLDHEIGHT / WINDOWHEIGHT) / game.world.scale.x;
-		var scaledY = rawY * (WORLDHEIGHT / WINDOWHEIGHT) / game.world.scale.y;
+		// get raw coordinates
+		var rawX = game.input.activePointer.worldX / game.world.scale.x;
+		var rawY = game.input.activePointer.worldY / game.world.scale.y;
+		// convert to scaled coordinates
+		var scaledX = rawX * (WORLDHEIGHT / WINDOWHEIGHT);
+		var scaledY = rawY * (WORLDHEIGHT / WINDOWHEIGHT);
+		// set leftClicked to false to allow another click
 		leftClicked = false;
+		// if width or height are negative, adjust value of coordinate and
+		//	flip sign of width/height
 		if (currentObject.rawWidth < 0) {
 			currentObject.rawX = currentObject.rawX + currentObject.rawWidth;
 			currentObject.scaledX = currentObject.scaledX + currentObject.scaledWidth;
@@ -167,16 +181,20 @@ function update() {
 			currentObject.rawHeight *= -1;
 			currentObject.scaledHeight *= -1;
 		}
+		// add the new rectangle to the array
 		boundaries.push(currentObject);
 	}
 
 	// handle right clicks
 	if (game.input.activePointer.rightButton.isDown && !rightClicked) {
 		rightClicked = true;
+		// clear screen
 		graphics.clear();
+		// delete last object in array
 		if (boundaries.length > 0) {
 			boundaries.splice(boundaries.length - 1);
 		}
+		// redraw objects
 		drawObjects();
 	}
 	else if (game.input.activePointer.rightButton.isUp && rightClicked) {
@@ -185,17 +203,24 @@ function update() {
 
 	// left click drag
 	if (leftClicked) {
+		// clear screen
 		graphics.clear();
+		// draw all objects
 		drawObjects();
-		graphics.lineStyle(4, 0xff0000, 1);
+		// update currently drawn object
+		graphics.lineStyle(LINEWIDTH, 0xff0000, 1);
+		// get raw coordinates
 		var rawX = game.input.activePointer.worldX / game.world.scale.x;
 		var rawY = game.input.activePointer.worldY / game.world.scale.y;
+		// convert to scaled coordinates
 		var scaledX = rawX * (WORLDHEIGHT / WINDOWHEIGHT);
 		var scaledY = rawY * (WORLDHEIGHT / WINDOWHEIGHT);
+		// set currentObjects values
 		currentObject.rawWidth = rawX - currentObject.rawX;
 		currentObject.rawHeight = rawY - currentObject.rawY;
 		currentObject.scaledWidth = scaledX - currentObject.scaledX;
 		currentObject.scaledHeight = scaledY - currentObject.scaledY;
+		// draw currentObject
 		graphics.drawRect(currentObject.rawX, currentObject.rawY, currentObject.rawWidth, currentObject.rawHeight);
 	}
 
@@ -209,17 +234,22 @@ function update() {
 	}
 }
 
+// draws all objects to the screen
 function drawObjects() {
+	// draw boundaries
 	for (var i = 0; i < boundaries.length; i++) {
-		graphics.lineStyle(4, 0xff0000, 1);
+		graphics.lineStyle(LINEWIDTH, 0xff0000, 1);
 		graphics.drawRect(boundaries[i].rawX, boundaries[i].rawY, boundaries[i].rawWidth, boundaries[i].rawHeight);
 	}
 }
 
+// mouse wheel event handler
 function mouseWheel (event) {
+	// if scrolling up, zoom in
 	if(game.input.mouse.wheelDelta > 0) {
 		game.world.scale.setTo(game.world.scale.x + CAMERAZOOMSPEED, game.world.scale.y + CAMERAZOOMSPEED);
 	}
+	// if scrolling down and you haven't zoomed out all the way, zoom out
 	else if(game.input.mouse.wheelDelta < 0 && game.world.scale.x > 1) {
 		game.world.scale.setTo(game.world.scale.x - CAMERAZOOMSPEED, game.world.scale.y - CAMERAZOOMSPEED);
 		if(game.world.scale.x < 1) {
@@ -228,6 +258,7 @@ function mouseWheel (event) {
 	}
 }
 
+// print all objects to console in JSON format
 function printAllObjects () {
 	var result = '{"boundaries":';
 	result += JSON.stringify(boundaries);
@@ -235,6 +266,7 @@ function printAllObjects () {
 	console.log(result);
 }
 
+// load objects from JSON string in txt file
 function loadObjects (filePath) {
 	var objectsString = readTextFile(filePath);
 	if (objectsString) {
@@ -249,6 +281,7 @@ function loadObjects (filePath) {
     }
 }
 
+// read text from file
 var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
 var response = "";
 function readTextFile(filePath) {
