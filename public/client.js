3d88d1843b83695b7ea437f68e35615a1a4498b7
Travis Stebbins <stebbins.travis@compute-linux1.cs.tamu.edu>
2016-11-14 22:30:46 -0600

    Fix merge conflicts

--- public/client.js
+++ public/client.js
@@ -1,9 +1,20 @@
 window.onload = function() {
 
-        var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+		var MAPPANELWIDTH = 3840;
+		var MAPPANELHEIGHT = 3347;
+		var SCALEFACTOR = 3;
+		var WORLDWIDTH = MAPPANELWIDTH * 2 * SCALEFACTOR;
+    	var WORLDHEIGHT = MAPPANELHEIGHT * 2 * SCALEFACTOR;
+    	var WINDOWWIDTH = 1280;
+    	var WINDOWHEIGHT = 720;
+
+    	var PLAYERSTARTX = 2904;
+    	var PLAYERSTARTY = 16102;
+
+        var game = new Phaser.Game(WINDOWWIDTH, WINDOWHEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
 
 		var socket;
-		var otherPlayers = [];
+		var otherPlayers;
         var player;
 
 		var angle = 0;
@@ -15,15 +26,15 @@ window.onload = function() {
     	var reversing = false;
     	var turnSpeed = 0.05;
 		var cooldown = 0;
-		var gotPow = 0;
 		var endText;
 
     	var obstacles;
+    	var boundaries;
 		function preload () {
-
 			game.load.image('logo', 'phaser.png');
 			game.load.image('bluebike', 'bluebike.png');
 			game.load.image('trashCan', 'trashCan.png');
+			game.load.image('powerUp', 'powerUp.png');
 			game.load.image('arrow', 'arrow.png');
 			// total map size: 7680 x 6694
 			game.load.image('mapTL', 'campusCircuit_TL.png');
@@ -32,16 +43,39 @@ window.onload = function() {
 			game.load.image('mapBR', 'campusCircuit_BR.png');
 			
 			game.load.image('finish', 'finishline.png');
-			game.load.image('powUp', 'pow.png');
 			
-			//otherPlayers = [];	//hold list of other players connected
+			otherPlayers = [];	//hold list of other players connected
+			create();			//load all of the objects onto the screen
 			
-			//load all of the objects onto the screen
 			//create soccket connection
-			
-			
-			
+			socket = io.connect({
+				'reconnection': true,
+				'reconnectionDelay': 1000,
+				'reconnectionDelayMax': 5000});
+			setEventHandlers();
+        }
 
+        function createObjects () {
+        	var objectsString = readTextFile("objects.txt");
+			if (!objectsString) {
+				objectsString = readTextFile("objects.txt");
+			}
+			if (!objectsString) {
+				objectsString = readTextFile("objects.txt");
+			}
+			if (objectsString) {
+				console.log("successful reading object file");
+				var objects = JSON.parse(objectsString);
+				var boundaryArray = objects.boundaries;
+				var obstacleArray = objects.obstacles;
+				var powerUpArray = objects.powerUps;
+        		createBoundaries(boundaryArray);
+        		createObstacles(obstacleArray);
+        		createPowerUps(powerUpArray);
+        	}        	
+			else {
+				console.log("error reading object file");
+			}
         }
 
 		function createObs(){
@@ -276,29 +310,67 @@ window.onload = function() {
 	        arrow.angle = 180;
 		}
 
+		function createBoundaries (boundaryArray) {
+			boundaries = game.add.group();
+			boundaries.enableBody = true;
+			var boundaryObjects = [];
+			var boundaryObject;
+			for (var i = 0; i < boundaryArray.length; i++) {
+				console.log("creating boundary at: " + boundaryArray[i].scaledX + ", " + boundaryArray[i].scaledY);
+				boundaryObject = boundaries.create(boundaryArray[i].scaledX, boundaryArray[i].scaledY);
+				boundaryObject.anchor.setTo(0, 0);
+				boundaryObject.width = boundaryArray[i].scaledWidth;
+				boundaryObject.height = boundaryArray[i].scaledHeight;
+				boundaryObject.body.immovable = true;
+			}
+		}
+
+		function createObstacles (obstacleArray) {
+			obstacles = game.add.group();
+			obstacles.enableBody = true;
+			var obstacleObjects = [];
+			var obstacleObject;
+			for (var i = 0; i < obstacleArray.length; i++) {
+				console.log("creating obstacle at: " + obstacleArray[i].scaledX + ", " + obstacleArray[i].scaledY);
+				obstacleObject = obstacles.create(obstacleArray[i].scaledX, obstacleArray[i].scaledY, 'trashCan');
+				obstacleObject.anchor.setTo(0, 0);
+				obstacleObject.body.immovable = true;
+			}
+		}
+
+		function createPowerUps (powerUpArray) {
+			powerUps = game.add.group();
+			powerUps.enableBody = true;
+			var powerUpObjects = [];
+			var powerUpObject;
+			for (var i = 0; i < powerUpArray.length; i++) {
+				console.log("creating power-up at: " + powerUpArray[i].scaledX + ", " + powerUpArray[i].scaledY);
+				powerUpObject = powerUps.create(powerUpArray[i].scaledX, powerUpArray[i].scaledY, 'powerUp');
+				powerUpObject.anchor.setTo(0, 0);
+				powerUpObject.body.immovable = true;
+			}
+		}
 	
-        function create () {				
-        	console.log("create");
-			socket = io.connect();
+        function create () {
 			// enable Arcade Physics system
 	        game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	        // set world size
-	        game.world.setBounds(0, 0, 23040, 20082);
-			game.desiredFps = 40;
+	        game.world.setBounds(0, 0, WORLDWIDTH, WORLDHEIGHT);
+
 	        // background (map)
 	        var mapTL = game.add.sprite(0, 0, 'mapTL');
 	        mapTL.anchor.setTo(0, 0);
-	        mapTL.scale.setTo(3, 3);
+	        mapTL.scale.setTo(SCALEFACTOR, SCALEFACTOR);
 	        var mapTR = game.add.sprite(game.world.width / 2, 0, 'mapTR');
 	        mapTR.anchor.setTo(0, 0);
-	        mapTR.scale.setTo(3, 3);
+	        mapTR.scale.setTo(SCALEFACTOR, SCALEFACTOR);
 	        var mapBL = game.add.sprite(0, game.world.height / 2, 'mapBL');
 	        mapBL.anchor.setTo(0, 0);
-	        mapBL.scale.setTo(3, 3);
+	        mapBL.scale.setTo(SCALEFACTOR, SCALEFACTOR);
 	        var mapBR = game.add.sprite(game.world.width / 2, game.world.height / 2, 'mapBR');
 	        mapBR.anchor.setTo(0, 0);
-	        mapBR.scale.setTo(3, 3);
+	        mapBR.scale.setTo(SCALEFACTOR, SCALEFACTOR);
 			
 			//finish line
 			finish = game.add.sprite(3100, 16065, 'finish');
@@ -309,16 +381,15 @@ window.onload = function() {
 	        createArrows();
 
 	        // player
-	        player = game.add.sprite(2900, 16150, 'bluebike');
+	        player = game.add.sprite(PLAYERSTARTX, PLAYERSTARTY, 'bluebike');
 	        player.anchor.setTo(0.5, 0.5);
 		    player.scale.setTo(0.5, 0.5);
 		    player.laps = 0;
 	        game.physics.arcade.enable(player);
 	        player.body.collideWorldBounds = true;
-           	player.body.bounce.setTo(5, 5); //lower numbers mean less bounce 
 
-	        // set up obstacles
-			createObs();
+	        // create objects
+	        createObjects();
 			
 			// set up powerup
 			powerUp = game.add.sprite(2900, 15000, 'powUp');
@@ -475,8 +546,90 @@ window.onload = function() {
 				game.debug.text("Got Power Up!!!", 32, 64);
 				powerUp.kill();
 				gotPow = 1;
-			}
+			}				
 			//powerUp.destroy();
+		}
+
+		var setEventHandlers = function() {//set all of the callback functions for socket events
+			socket.on('connect', onSocketConnected);//new connection
+			
+			socket.on('newPlayer', onNewPlayer);//new player
+			
+			socket.on('movePlayer', onMovePlayer);//send a new id to the new player
+			socket.on('disconnect', onSocketDisconnect);//one of the players has moved
+			socket.on('removePlayer', onRemovePlayer);//player disconnected
+			socket.on('playerID', function(data) {
+				player.id = data.id;
+				player.playerNum = data.playerNum;
+				player.x += (player.playerNum*35); 
+			});
+			socket.on('gameFinish', onGameFinish);//remove player from game
+
+		}
+		
+		function onSocketConnected() {
+			console.log('Connected to socket server');
+			
+			socket.emit('newPlayer', { x: player.x, y: player.y, angle: player.angle });	//send server info to create new player
+	
+		}
+
+		function onSocketDisconnect () {
+ 			console.log('Disconnected from socket server');
+		}
+		
+		function setPlayerId(data) {
+			player.id = data.id;
+			player.playerNum = data.playerNum;	//index in the servers player list
+			player.x += (player.playerNum*35);	//set starting position based on how many people are connected
+		}
+		
+		function onNewPlayer (data) {
+			console.log('New player connected:', data.id);
+
+			// Avoid possible duplicate players
+			var duplicate = playerById(data.id);
+			if (duplicate) {
+				console.log('Duplicate player!');
+				return;
+			}
+			if (player.id == data.id){
+				return;
+			}
+			
+			// Add new player to the remote players array
+			otherPlayers.push(new OtherPlayer(data.id, game, player, data.playerNum, data.x, data.y, data.angle));	//create new player and put it into list of current players
+			console.log(otherPlayers);	//debugging
+		}
+		
+		function onMovePlayer (data) {
+			var movePlayer = playerById(data.id);	//get the player that is currently being moved
+
+		  // Player not found
+			if (!movePlayer) {
+				console.log('Player not found: ', data.id);
+				return
+			}
+
+			// Update player position
+			movePlayer.player.x = data.x;
+			movePlayer.player.y = data.y;
+			movePlayer.player.angle = data.angle;
+			movePlayer.player.laps = data.laps;
+		}
+		
+		function onGameFinish (data) {
+			endText.setText("You Lose!");
+			endText.anchor.setTo(0.5, 0.5);
+		}
+		
+		function checkOverlap(spriteA, spriteB) {
+
+			var boundsA = spriteA.getBounds();
+			var boundsB = spriteB.getBounds();
+
+			return Phaser.Rectangle.intersects(boundsA, boundsB);
+
 		}
 		
 		function update() {
@@ -580,4 +733,3 @@ window.onload = function() {
 		}
 		
     };
-
